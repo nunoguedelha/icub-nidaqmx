588203e7f69d1cb1791960ba96c3f02d0cd65a1d
Francesco Giovannini <francesco.giovannini@inria.fr>
2014-07-18 11:29:18 +0200

    [TRAVIS] Fixing path to nidaqmx library and headers.

--- .nidaqmx/copy-nidaqmx-to-root.sh
+++ .nidaqmx/copy-nidaqmx-to-root.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Copy NIDAQmx headers and libs to system root so that Travis builds are correctly executed.
-sudo cp -R ./usr /
+echo -e "Copying library and header files to system root. \n"
+
+cd $PROJ_ROOT
+sudo cp -R ./nidaqmx/usr /
+
+ls -al /usr/local/lib/
+ls -al /usr/local/include/
+
+echo -e "Done. \n"
