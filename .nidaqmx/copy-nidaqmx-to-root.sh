85b474ea7961cd3a8d9a181a791d8dd1dac28a80
Francesco Giovannini <francesco.giovannini@inria.fr>
2014-07-18 12:27:24 +0200

    [TRAVIS] Removing ls commands used for debugging.

--- .nidaqmx/copy-nidaqmx-to-root.sh
+++ .nidaqmx/copy-nidaqmx-to-root.sh
@@ -4,14 +4,10 @@
 echo -e "Copying library and header files to system root. \n"
 
 cd $PROJ_ROOT
-ls -al
 sudo cp -R ./.nidaqmx/usr /
 if [ $? -gt 0 ]; then
     echo ERROR
     exit 1
 fi
-
-ls -al /usr/local/lib/
-ls -al /usr/local/include/
 
 echo -e "Done. \n"
